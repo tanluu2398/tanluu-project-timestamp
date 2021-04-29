307fbd3c692ea86e209848dddbfe4d98075b940e
tanluu2398 <>
2021-04-29 03:47:01 +0000

    First commit

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/timestamp/:slug", function (req, res) {
+  var date = req.params.slug?Date(req.params.slug):new Date();
+  console.log(date);
   res.json({greeting: 'hello API'});
 });
 
